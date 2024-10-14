f4e4f975a76018c2e12aeae1adc557d287a56a58
ahmed <am3450931@gmail.com>
2024-10-14 18:55:00 +0300

    update server url

--- src/components/Orderonline/Basket.jsx
+++ src/components/Orderonline/Basket.jsx
@@ -30,7 +30,7 @@ export default function Basket({ basket, setBasket }) {
     if (basket.length > 0) {
       try {
         // Send basket data to the backend
-        const response = await fetch('http://localhost:5000/checkout', {
+        const response = await fetch('https://restaurant-server-beta.vercel.app/checkout', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
